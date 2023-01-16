f13b570942b27e8611559653637a4fd2571bb53b
koki-develop <kou.pg.0131@gmail.com>
2023-01-16 15:10:03 +0900

    get image name

--- cmd/root.go
+++ cmd/root.go
@@ -1,14 +1,19 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
-	Use: "docker-tags",
+	Use:  "docker-tags",
+	Args: cobra.MinimumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
+		img := args[0]
+		fmt.Println(img)
+
 		return nil
 	},
 }
