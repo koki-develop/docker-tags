4ab7471ce64c548e3612b31a46b1b74a84ed0455
koki-develop <kou.pg.0131@gmail.com>
2023-01-17 12:29:57 +0900

    add google auth

--- go.mod
+++ go.mod
@@ -6,12 +6,18 @@ require (
 	github.com/aws/aws-sdk-go v1.44.180
 	github.com/distribution/distribution v2.8.1+incompatible
 	github.com/spf13/cobra v1.6.1
+	golang.org/x/oauth2 v0.4.0
 )
 
 require (
+	cloud.google.com/go/compute/metadata v0.2.0 // indirect
 	github.com/docker/distribution v2.8.1+incompatible // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/net v0.5.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
 )
--- pkg/artifactregistry/client.go
+++ pkg/artifactregistry/client.go
@@ -1,17 +1,22 @@
 package artifactregistry
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
 	"net/http"
 	"net/url"
 	"path"
+
+	"golang.org/x/oauth2/google"
 )
 
 type Client struct {
 	domain     string
 	httpClient *http.Client
+	token      string
 }
 
 type Config struct {
@@ -41,6 +46,9 @@ func (cl *Client) ListTags(name string) ([]string, error) {
 	if err != nil {
 		return nil, err
 	}
+	if err := cl.auth(name); err == nil && cl.token != "" {
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", cl.token))
+	}
 
 	resp, err := cl.httpClient.Do(req)
 	if err != nil {
@@ -67,4 +75,54 @@ func (cl *Client) ListTags(name string) ([]string, error) {
 	}
 
 	return tags, nil
+}
+
+type dockerAuthResponse struct {
+	Token string `json:"token"`
+}
+
+func (cl *Client) auth(name string) error {
+	ctx := context.Background()
+	cred, err := google.FindDefaultCredentials(ctx)
+	if err != nil {
+		return err
+	}
+
+	tkn, err := cred.TokenSource.Token()
+	if err != nil {
+		return err
+	}
+
+	u := url.URL{Scheme: "https", Path: path.Join(cl.domain, "/v2/token")}
+	q := u.Query()
+	q.Set("scope", fmt.Sprintf("repository:%s:pull", name))
+	u.RawQuery = q.Encode()
+
+	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
+	if err != nil {
+		return err
+	}
+	req.SetBasicAuth("_token", tkn.AccessToken)
+
+	resp, err := cl.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		b, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return err
+		}
+		return errors.New(string(b))
+	}
+
+	var authResp dockerAuthResponse
+	if err := json.NewDecoder(resp.Body).Decode(&authResp); err != nil {
+		return err
+	}
+
+	cl.token = authResp.Token
+	return nil
 }
